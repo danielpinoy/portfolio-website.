f77951f070b81c25a1c8d148a43caf1b4bb57025
danielpinoy <144582323+danielpinoy@users.noreply.github.com>
2024-03-14 22:15:50 +0800

    3-14-2024 Changes
    -minified javascript
    -minified css
    -misc changes to all html

--- js/simple.js
+++ js/simple.js
@@ -1,22 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
     const gridItems = document.querySelectorAll(".grid-item");
 
-    gridItems.forEach(function (item) {
+    gridItems.forEach((item) => {
         item.addEventListener("click", function () {
             this.classList.toggle("selected");
 
             const paragraph = this.querySelector("p");
+            const bulletPoints = this.querySelectorAll("li");
+
             if (paragraph) {
                 paragraph.classList.toggle("visible");
             }
 
-            gridItems.forEach(function (gridItem) {
+            bulletPoints.forEach((bulletPoint) => {
+                bulletPoint.classList.toggle("visible");
+                bulletPoint.style.display = bulletPoint.classList.contains("visible")
+                    ? "list-item"
+                    : "none";
+            });
+
+            // Toggle hidden class for all grid items except the clicked one
+            gridItems.forEach((gridItem) => {
                 if (gridItem !== item) {
                     gridItem.classList.toggle("hidden");
                     const otherParagraph = gridItem.querySelector("p");
+
                     if (otherParagraph) {
                         otherParagraph.classList.toggle("visible");
                     }
+                }
+            });
+            // toggle remove chat and todo class
+            gridItems.forEach(function (gridItem) {
+                if (gridItem !== item && gridItem.classList.contains("selected")) {
+                    gridItem.classList.remove("chat", "todo");
                 }
             });
         });
